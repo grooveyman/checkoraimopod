f697ef5673d5343ebeaed1a5731e800e6d2dee77
unknown <edegraftamaning85@gmail.com>
2023-06-03 10:30:39 +0000

    Initial commit

---
+++ cronjob.py
@@ -0,0 +1,35 @@
+import datetime
+import requests
+import os
+from bs4 import BeautifulSoup
+from tkinter import *
+from winotify import Notification, audio
+
+toast = Notification(app_id="Oraimo Checker", title="Oraimo Pod Availability"
+                     ,msg="oraimo FreePods 3 TWS True Wireless Stereo Earbuds is now avalable",
+                     duration="long",
+                     icon=f"{os.getcwd()}\oraimo.png")
+toast.set_audio(audio.Default, loop=False)
+
+toast.add_actions(label="Order Now!", launch="https://gh.oraimo.com/oraimo-freepods-3-true-wireless-earbuds.html")
+
+
+
+file = open(f'{os.getcwd()}\task.txt', 'a') #a option is for append
+
+
+url = "https://gh.oraimo.com/oraimo-freepods-3-true-wireless-earbuds.html"
+response = requests.get(url)
+
+content = response.content
+parsed_content = BeautifulSoup(content, 'html.parser')
+main_content = parsed_content.find("span", {"itemprop":"name"}).get_text()
+
+if(main_content == 'oraimo FreePods 3 TWS True Wireless Stereo Earbuds'):
+    stock = parsed_content.find("button",{"id":"product-addtocart-button"}).findNext("span").get_text()
+    if(stock.lower() != "Out of Stock".lower()):
+        #show notification
+        toast.show()
+        file.write(f'{datetime.datetime.now()} - Available Now \n')
+    else:
+        file.write(f'{datetime.datetime.now()} - Not available \n')
